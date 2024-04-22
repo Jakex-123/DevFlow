ee801365cb6ebff5c653122f5dc48f7417afb69e
Aditya <aditya1282003@gmail.com>
2024-04-23 01:21:03 +0530

    upgraded next.js version

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,32 @@
 'use server'
 
+import Question from "@/database/question.model";
 import { connectDB } from "../mongoose"
+import Tag from "@/database/tag.model";
 
-export async function createQuestion(params:any){
+export async function createQuestion({params}:any){
     try{
         connectDB();
+        const {title, content,tags,author,path}=params;
+
+        const question=await Question.create({
+            title,author,content
+        })
+
+        const tagDocuments=[]
+        for (const tag of tags){
+            const existingTag=await Tag.findOneAndUpdate(
+                {name:{ $regex: new RegExp(`^${tag}$`,"i")}},//find i=case-insensitive
+                {$setOnInsert:{name:tag}, $push:{ question:question._id}},//do something on it
+                {upsert: true,new:true}//attributes
+            )
+            tagDocuments.push(existingTag._id)
+        }
+
+        await Question.findByIdAndUpdate(question._id,{
+            $push:{tags:{$each:tagDocuments}}
+        })
+
     }
     catch(err){
 
