58e3561edf0a9f0df7f73e8995a3f689192fdc44
Aditya Gupta <aditya1282003@gmail.com>
2026-02-14 12:44:30 +0530

    Fix indentation for publishableKey in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,8 +42,8 @@ export default function RootLayout({
                 formButtonPrimary: 'primary-gradient',
                 footerActionLink: 'primary-text-gradient hover:text-primary-500'
               },
-              publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
             }}
+            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
           >
             <ThemeProvider>
               {children}
